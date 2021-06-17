b7f36459c5cd9dbb73700ef2c7bb5ac3a87fbb14
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2021-06-17 14:36:07 -0300

    Update Ajuste.py

--- Ajuste.py
+++ Ajuste.py
@@ -2,7 +2,7 @@
 Funções para ajustar a base de dados nos padrões do FAO 56.
 """
 
-def conversao_U2(dataset, altitude):
+def conversao_U2(dataset):
     """
     Conversão da velocidade do vento medida a 10m para 2m com limite de 0.5 m/s
     Equação 47 (FAO 56)
@@ -11,7 +11,7 @@ def conversao_U2(dataset, altitude):
     """
     for i in range(dataset.shape[0]-1): 
       if np.isnan(dataset.loc[i]) == False:
-        dataset.loc[i] = dataset.loc[i] * (4.87 / math.log(67.8 * altitude - 5.42)) 
+        dataset.loc[i] = dataset.loc[i] * (4.87 / math.log(67.8 * 10 - 5.42)) 
         if dataset.loc[i] < .5:
           dataset.loc[i] = .5
           
